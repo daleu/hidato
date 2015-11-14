d92e06ae776cbf9510ca1665906938d5317427f3
vylion <volfaria@gmail.com>
2015-11-14 11:23:47 +0100

    V3: * Creado un timer llamado Contador, que cuenta hacia arriba o hacia abajo segun si se le da un valor inicial o no. * Anadido una nueva linea en el .gitignore para el nuevo proyecto DriverTimer.

---
+++ DriverTimer/src/Contador.java
@@ -0,0 +1,48 @@
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Contador {
+	private int segundos;
+	private Timer cronometro;
+	private boolean countdown;
+	
+	private class Segundera extends TimerTask {
+		public void run() {
+			if(!countdown) segundos++;
+			else segundos--;
+		}
+	}
+	
+	public Contador() {
+		segundos = 0;
+		cronometro = new Timer();
+		countdown = false;
+	}
+	
+	public Contador(int s) {
+		segundos = s;
+		cronometro = new Timer();
+		countdown = true;
+	}
+	
+	public Contador(int m, int s) {
+		this(m*60 + s);
+	}
+	
+	public Contador(int h, int m, int s) {
+		this(h*3600 + m*60 + s);
+	}
+	
+	public void Iniciar() {
+		cronometro.schedule(new Segundera(), segundos, 1000);
+	}
+	
+	public void Detener() {
+		cronometro.cancel();
+	}
+	
+	public int getSegundos() {
+		return segundos;
+	}
+}
