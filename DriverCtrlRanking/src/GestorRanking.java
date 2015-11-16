13f7cafe85a7c070c6e6b2a8876bc02ad82b8668
vylion <volfaria@gmail.com>
2015-11-16 12:08:28 +0100

    V6: * Creado CtrlRanking * Creado DriverCtrlRanking (no probado)

---
+++ DriverCtrlRanking/src/GestorRanking.java
@@ -0,0 +1,17 @@
+
+public class GestorRanking {
+	
+	public GestorRanking() {
+		
+	}
+	
+	public void store(Ranking r) {
+		
+	}
+	
+	public Ranking load() {
+		Ranking r = null;
+		
+		return r;
+	}
+}
